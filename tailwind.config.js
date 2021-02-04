4c8e5f3d26d62ed123e58f29eef4be712293f035
Demola Malomo <demolamalomo@Demolas-MBP.lan>
2021-02-04 12:16:16 +0100

    initial design system setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@ module.exports = {
 	purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
 	darkMode: false, // or 'media' or 'class'
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				'brand-blue': '#1E3A8A',
+				'brand-black': '#181B25',
+				'brand-grey': '#5E616A',
+				'brand-white': '#FDFDFF',
+				'brand-blue-hover': '#0A277A',
+			},
+		},
+		fontFamily: {
+			display: ['Open Sans', 'sans-serif'],
+			body: ['Open Sans', 'sans-serif'],
+		}
 	},
 	variants: {
 		extend: {},
